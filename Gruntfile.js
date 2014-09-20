d2684f4e951fdb9ffef97788913c9585ab068639
Анастасия Сахарова <elliot.wrt@gmail.com>
2014-09-21 00:51:46 +0600

    open new branch

---
+++ Gruntfile.js
@@ -0,0 +1,27 @@
+module.exports = function(grunt) {
+
+  require('load-grunt-tasks')(grunt); // npm install --save-dev load-grunt-tasks
+
+  grunt.initConfig({
+      sass: {
+          options: {
+              sourceMap: true
+          },
+          dist: {
+              files: {
+                  'static/styles/output.css': 'static/styles/main.scss'
+              }
+          }
+      },
+
+      watch: {
+        sass: {
+            files: ['static/styles/*.scss'],
+            tasks: ['sass']
+        }
+      }
+  });
+
+  grunt.registerTask('default', ['sass', 'watch']);
+
+};
